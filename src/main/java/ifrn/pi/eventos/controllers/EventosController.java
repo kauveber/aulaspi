ec48fa3f37d1bdc7d7eecff92162dd997450fdc7
Kau-Veber <kauveber@gmail.com>
2021-12-07 21:20:26 -0300

    adicionando banco de dados

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,26 +1,31 @@
 package ifrn.pi.eventos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.EventoRepository;
 
 @Controller
 public class EventosController {
+	
+	@Autowired
+	private EventoRepository er;
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
 	}
 	
-	@RequestMapping("/resp/form")
-	public String formResp(Evento evento) {
-		System.out.println("Formulario enviado");
-		System.out.println("Nome: "+evento.getNome());
-		System.out.println("Local: "+evento.getLocal());
-		System.out.println("Data: "+evento.getData());
-		System.out.println("Horario: "+evento.getHorario());
-		return "formEvento";
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+	
+		System.out.println(evento);
+		er.save(evento);
+		
+		return "evento-adicionado";
 	}
 	
 }
