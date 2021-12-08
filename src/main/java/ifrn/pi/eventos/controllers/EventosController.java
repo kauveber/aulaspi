3c995d23cc6e809c3c884053ed924b45d65fd189
Kau-Veber <kauveber@gmail.com>
2021-12-08 20:13:34 -0300

    adicionando pagina detalhes

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,10 +1,12 @@
 package ifrn.pi.eventos.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,24 +17,24 @@ import ifrn.pi.eventos.repositories.EventoRepository;
 @Controller
 @RequestMapping("/eventos")
 public class EventosController {
-	
+
 	@Autowired
 	private EventoRepository er;
-	
+
 	@GetMapping("/form")
 	public String form() {
 		return "eventos/formEvento";
 	}
-	
+
 	@PostMapping
 	public String adicionar(Evento evento) {
-	
+
 		System.out.println(evento);
 		er.save(evento);
-		
+
 		return "eventos/evento-adicionado";
 	}
-	
+
 	@GetMapping
 	public ModelAndView listar() {
 		List<Evento> eventos = er.findAll();
@@ -40,5 +42,22 @@ public class EventosController {
 		mv.addObject("eventos", eventos);
 		return mv;
 	}
-	
+
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Evento> opt = er.findById(id);
+
+		if (opt.isEmpty()) {
+			md.setViewName("redirect:/listar");
+			return md;
+		}
+
+		md.setViewName("eventos/detalhes");
+		Evento evento = opt.get();
+		md.addObject("evento", evento);
+
+		return md;
+	}
+
 }
