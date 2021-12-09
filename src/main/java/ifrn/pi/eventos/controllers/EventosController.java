70959cc321e482f43f9bf68dfc5ca91e1ae8f25c
Kau-Veber <kauveber@gmail.com>
2021-12-08 22:24:40 -0300

    listando convidados

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -61,6 +61,9 @@ public class EventosController {
 		Evento evento = opt.get();
 		md.addObject("evento", evento);
 
+		List<Convidado> convidados = cr.findByEvento(evento);
+		md.addObject("convidados", convidados);
+		
 		return md;
 	}
 
